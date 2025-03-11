6669b9d1675d52424db3605c3441cec9c8479ec6
lonhb0124 <lonhb0124@gmail.com>
2025-03-11 16:47:54 -0400

    spi_update

--- Debug/Src/spi_test.d
+++ Debug/Src/spi_test.d
@@ -1,4 +1,4 @@
-Src/main.o: ../Src/main.c \
+Src/spi_test.o: ../Src/spi_test.c \
  C:/Users/Hyunbin/STM32CubeIDE/workspace_1.16.1/target_3/stm32f302x8_drivers/drivers/Inc/stm32f302x8.h \
  C:/Users/Hyunbin/STM32CubeIDE/workspace_1.16.1/target_3/stm32f302x8_drivers/drivers/Inc/stm32f302x8_gpio_drivers.h \
  C:/Users/Hyunbin/STM32CubeIDE/workspace_1.16.1/target_3/stm32f302x8_drivers/drivers/Inc/stm32f302x8.h \
--- drivers/Inc/stm32f302x8_spi_drivers.h
+++ drivers/Inc/stm32f302x8_spi_drivers.h
@@ -14,7 +14,7 @@ typedef struct {
 	uint8_t SPI_Device_Mode;
 	uint8_t SPI_Bus_Config;
 	uint8_t SPI_SCLK_Speed;
-	uint8_t SPI_DFF;
+	uint8_t SPI_CRCL;
 	uint8_t SPI_CPOL;
 	uint8_t SPI_CPHA;
 	uint8_t SPI_SSM;
@@ -24,6 +24,59 @@ typedef struct {
 	SPI_REG_t *pSPIx;
 	SPI_Config_t SPI_Config;
 } SPIx_Handle_t;
+
+/* SPI MODE */
+#define SPI_DEVICE_MODE_S	0
+#define SPI_DEVICE_MODE_M	1
+
+
+/* SPI BUS */
+#define SPI_BUS_FULL_DUPLEX	1
+//#define SPI_BUS_HALF_DUPLEX	2
+#define SPI_BUS_SIMPLEX_TX_ONLY	3
+#define SPI_BUS_SIMPLEX_RX_ONLY	4
+
+/* SPI SCLK SPEED */
+#define SPI_SCLK_SPEED_DIV_2	0
+#define SPI_SCLK_SPEED_DIV_4	1
+#define SPI_SCLK_SPEED_DIV_8	2
+#define SPI_SCLK_SPEED_DIV_16	3
+#define SPI_SCLK_SPEED_DIV_32	4
+#define SPI_SCLK_SPEED_DIV_64	5
+#define SPI_SCLK_SPEED_DIV_128	6
+#define SPI_SCLK_SPEED_DIV_256	7
+
+/* SPI CRCL */
+#define SPI_CRCL_8_BITS		0
+#define SPI_CRCL_16_BITS		1
+
+/* SPI CPLO */
+#define SPI_CPLO_LOW		0
+#define SPI_CPLO_HIGH		1
+
+/* SPI CPHA */
+#define SPI_CPHA_LOW		0
+#define SPI_CPHA_HIGH		1
+
+/* SPI SSM */
+#define SPI_SSM_SW_DI			0
+#define SPI_SSM_SW_EN			1
+
+
+/* SPI flag */
+#define SPI_RXNE_FLAG			(1 << SPI_SR_RXNE)
+#define SPI_TXE_FLAG			(1 << SPI_SR_TXE)
+#define SPI_CHSIDE_FLAG			(1 << SPI_SR_CHSIDE)
+#define SPI_UDR_FLAG			(1 << SPI_SR_UDR)
+#define SPI_CRCERR_FLAG			(1 << SPI_SR_CRCERR)
+#define SPI_MODF_FLAG			(1 << SPI_SR_MODF)
+#define SPI_OVR_FLAG			(1 << SPI_SR_OVR)
+#define SPI_BUSY_FLAG			(1 << SPI_SR_BSY)
+#define SPI_FRE_FLAG			(1 << SPI_SR_FRE)
+#define SPI_FRLVL_FLAG			(1 << SPI_SR_FRLVL)
+#define SPI_FTLVL_FLAG			(1 << SPI_SR_FTLVL)
+
+
 
 /* Clock */
 void SPI_PCLK_CTRL(SPI_REG_t *pSPIx, uint8_t EnDi);
@@ -40,6 +93,9 @@ void SPI_Receive_Data(SPI_REG_t *pSPIx, uint8_t *pTx_Buffer, uint32_t Len);
 void SPI_IRQ_Interrupt_Config(uint8_t IRQNumber, uint8_t EnDi);
 void SPI_IRQ_Priority_Config(uint8_t IRQNumber, uint32_t IRQPriority);
 void SPI_IRQ_Handling(SPIx_Handle_t *pSPIHandle);
+
+/* */
+void SPI_Peri_CTRL(SPI_REG_t *pSPIx, uint8_t EnDi);
 
 
 #endif /* INC_STM32F302X8_SPI_DRIVERS_H_ */
